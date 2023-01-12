510dd3f75c83cb390b9486cffa2f2699faa2bf6b
bitnaleeeee <bitnaleeeee@gmail.com>
2023-01-12 20:21:58 +0900

    localstarge저장

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+
+    export default {
+      data() {
+        return {
+          newTodoItem: ''
+        }
+      },
+      methods: {
+        addTodo() {
+          if (this.newTodoItme !== "") {
+            let value = this.newTodoItem && this.newTodoItem.trim();
+            localStorage.setItem(value, value);
+            this.celarInput();
+          }
+        },
+        clearInput() {
+          this.newTodoItme = '';
+        }  
+      }
+    }
+</script>
+
+<style>
+</style>
